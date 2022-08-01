8f62959df62fb17266395675180a3d7e490102bd
Mike Cousins <mike@mikecousins.com>
2022-08-01 16:58:28 -0600

    Switching to Link components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,18 @@
+import Link from 'next/link';
 import LoginButton from './LoginButton';
 
 const Header = () => (
   <div className="w-full p-4">
     <div className="float-right flex">
-      <a className="border p-2 rounded mr-2" href="/settings">
-        Settings
-      </a>
+      <Link href="/">
+        <a className="font-bold p-2 rounded mr-2">Home</a>
+      </Link>
+      <Link href="/progress">
+        <a className="font-bold p-2 rounded mr-2">Progress</a>
+      </Link>
+      <Link href="/settings">
+        <a className="font-bold p-2 rounded mr-2">Settings</a>
+      </Link>
       <LoginButton />
     </div>
   </div>
