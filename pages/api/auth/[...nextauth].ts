0e4ea613e8590576b9e4f4c1d0fc89689fea018b
Mike Cousins <mike@mikecousins.com>
2022-08-01 13:00:20 -0600

    Working on prisma/next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
+import { PrismaClient } from '@prisma/client';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import LoginButton from '../components/LoginButton';
 
 const Home: NextPage = () => {
   return (
     <div className="container mx-auto">
       <div className="text-2xl font-bold">Foos</div>
+      <LoginButton />
     </div>
   );
 };
