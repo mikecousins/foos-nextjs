329dfba81c37f286ca43447319acbc316e3e1858
Mike Cousins <mike@mikecousins.com>
2022-08-02 22:20:50 -0600

    Adding more data loading

--- pages/progress.tsx
+++ pages/progress.tsx
@@ -22,21 +22,37 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     return { props: {} };
   }
 
-  const response = await fetch(
-    'https://api.passiv.com/api/v1/portfolioGroups',
-    { headers: { Authorization: `Token ${user.passivToken}` } }
-  );
+  const headers = { Authorization: `Token ${user.passivToken}` };
 
-  const portfolioGroups = await response.json();
+  const portfolioGroups = await fetch(
+    'https://api.passiv.com/api/v1/portfolioGroups', { headers }
+  ).then(response => response.json());
 
-  return { props: { portfolioGroups } };
+  const getGroupInfo = async (id: any) => {
+    return await fetch(`https://api.passiv.com/api/v1/portfolioGroups/${id}/info`, { headers }).then(response => response.json());
+  }
+
+  const getGroups = async () => {
+    return Promise.all(portfolioGroups.map((group: any) => getGroupInfo(group.id)));
+  }
+
+  const groupDetails = await getGroups();
+
+  const goals = await fetch('https://api.passiv.com/api/v1/goals/', { headers }).then(response => response.json());
+  const performance = await fetch('https://api.passiv.com/api/v1/performance/all/', { headers }).then(response => response.json());
+
+  return { props: { portfolioGroups, goals, performance, groupDetails } };
 };
 
 type Props = {
   portfolioGroups: any;
+  goals: any;
+  performance: any;
+  groupDetails: any;
 };
 
-const Progress: NextPage<Props> = ({ portfolioGroups }) => {
+const Progress: NextPage<Props> = ({ portfolioGroups, goals, performance, groupDetails }) => {
+  console.log(portfolioGroups, goals, performance, groupDetails);
   return (
     <>
       <div className="text-2xl font-bold">Progress</div>
